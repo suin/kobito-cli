4adb333644083a950994c5fa2d64eddd36d66e08
suin <suinyeze@gmail.com>
2014-10-16 16:44:05 +0900

    support Kobito 2.0.0

--- port-sqlite3-init.go
+++ port-sqlite3-init.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"os"
 	"os/user"
 )
 
 func init() {
 	usr, err := user.Current()
 	failOnError(err)
-	itemRepository = NewSqliteItemRepository(usr.HomeDir + "/Library/Kobito/Kobito.db")
+
+	// Kobito version 2.x
+	kobito2 := usr.HomeDir + "/Library/Containers/com.qiita.Kobito/Data/Library/Kobito/Kobito.db"
+	// Kobito version 1.x
+	kobito1 := usr.HomeDir + "/Library/Kobito/Kobito.db"
+
+	var kobitoDbFile string
+
+	if _, err := os.Stat(kobito2); err == nil {
+		kobitoDbFile = kobito2
+	} else {
+		kobitoDbFile = kobito1
+	}
+
+	itemRepository = NewSqliteItemRepository(kobitoDbFile)
 }
